46ec380d4d1b6fbd23deb0a4ce281e169f1d38f5
Kanay <kanayg@icloud.com>
2021-07-15 20:49:41 +1000

    Added Player Movement

--- EscapeRoom/Assets/Scripts/MouseLook.cs
+++ EscapeRoom/Assets/Scripts/MouseLook.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    public float mouseSensitivty = 10;
+    public Transform playerBody;
+
+    float xRotation = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X")
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivty * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivty * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
